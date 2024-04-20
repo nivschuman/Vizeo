5c8f6d114839eb6ae244b29e659c19f359e8d7b8
niv <niv.schuman@gmail.com>
2024-04-20 16:55:46 +0300

    html design
    created basic html design

--- obj/Debug/net8.0/VideoProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/VideoProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92fe3f54418e54e64ea672530b87eed00cc317c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
