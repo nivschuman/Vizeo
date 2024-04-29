8ceaaf73386d73ce9c99f46735b565ac04413cd5
niv <niv.schuman@gmail.com>
2024-04-29 18:35:27 +0300

    ice candidate passing
    changed connection between 2 users, added passing of ice candidates

--- obj/Debug/net8.0/VideoProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/VideoProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cac04b5f4efa5481300574bcac6b4f40b14042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f653ec196840173cf565ecda40709ade03825d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
