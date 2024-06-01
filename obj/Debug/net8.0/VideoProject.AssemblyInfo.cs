9789d1c47243e62c3d42d64b8faa2a92c1b031f1
niv <niv.schuman@gmail.com>
2024-06-01 22:07:55 +0300

    buttons enable disable
    added enabling and disabling of go stop and next buttons

--- obj/Debug/net8.0/VideoProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/VideoProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1abe8707022bea0d77449f0802a494036e94a879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b082f44eb42cf3a5f6615fe70dc44fe75f253b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
